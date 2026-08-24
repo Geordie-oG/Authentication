da313861a4928a2f1af3e0d85cd5543c7336efed
shinthant-aung <shinthantaung9806@gmail.com>
2026-08-24 15:20:25 +0700

    Refresh repository

---
+++ hello_world/src/app/api/mongo_test/route.js
@@ -0,0 +1,19 @@
+import corsHeaders from "@/lid/cors";
+import { getClientPromise } from "@/lid/mongodb";
+
+import { NextResponse } from "next/server"; 
+
+export async function GET() { 
+    const client = await getClientPromise(); 
+
+    const db = client.db("sample_mflix"); 
+
+    const result = await db.collection("comments").find({}).skip(0).limit(10).toArray(); 
+
+    return NextResponse.json(result, { 
+
+        headers: corsHeaders 
+
+    }); 
+
+} 
